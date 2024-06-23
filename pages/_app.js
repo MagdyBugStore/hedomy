ff652fd3526c06eefc8dc6dff53900a1f1c00e6d
Ahmed magdy <ahmedmegaa1@gmail.com>
2024-06-23 19:18:35 +0300

    test8

--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,28 @@
 import { useEffect, useState } from "react";
 // import "react-input-range/lib/css/index.css";
-import "react-perfect-scrollbar/dist/css/styles.css";
 import { Provider } from "react-redux";
+import "react-perfect-scrollbar/dist/css/styles.css";
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import "react-responsive-modal/styles.css";
-
-import "swiper/css";
-import "swiper/css/navigation";
 import StorageWrapper from "../components/ecommerce/storage-wrapper";
 import store from "../redux/store";
 import Preloader from "./../components/elements/Preloader";
+import 'react-toastify/dist/ReactToastify.css';
+import "react-responsive-modal/styles.css";
+import "swiper/css";
+import "swiper/css/navigation";
 
 
 
 function MyApp({ Component, pageProps }) {
 
+    const [loading, setLoading] = useState(true);
 
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-        setLoading(true);
-        setTimeout(() => {
-            setLoading(false);
-        }, 2000);
-    }, []);
     return (
         <>
             {!loading ? (
                 <Provider store={store}>
-                    <StorageWrapper>
                         <Component {...pageProps} />
                         <ToastContainer />
-                    </StorageWrapper>
                 </Provider>
             ) : (
                 <Preloader />
