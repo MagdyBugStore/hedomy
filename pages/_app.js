4b1d53af1d95d4519e6190ffd2fd635d323eab4f
Ahmed magdy <ahmedmegaa1@gmail.com>
2024-06-23 05:34:50 +0300

    test2

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
-// import "react-input-range/lib/css/index.css";
-import "react-perfect-scrollbar/dist/css/styles.css";
-import { Provider } from "react-redux";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import "react-responsive-modal/styles.css";
+// // import "react-input-range/lib/css/index.css";
+// import "react-perfect-scrollbar/dist/css/styles.css";
+// import { Provider } from "react-redux";
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import "react-responsive-modal/styles.css";
 
-import "swiper/css";
-import "swiper/css/navigation";
-import StorageWrapper from "../components/ecommerce/storage-wrapper";
-import store from "../redux/store";
-import Preloader from "./../components/elements/Preloader";
-import "../public/assets/css/main.css";
+// import "swiper/css";
+// import "swiper/css/navigation";
+// import StorageWrapper from "../components/ecommerce/storage-wrapper";
+// import store from "../redux/store";
+// import Preloader from "./../components/elements/Preloader";
+// import "../public/assets/css/main.css";
 
 
 
