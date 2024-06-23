ab6af37262bf8b624442a29cfb7868236d96c85b
Ahmed magdy <ahmedmegaa1@gmail.com>
2024-06-23 17:53:08 +0300

    test3

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
-// // import "react-input-range/lib/css/index.css";
-// import "react-perfect-scrollbar/dist/css/styles.css";
-// import { Provider } from "react-redux";
-// import { ToastContainer } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-// import "react-responsive-modal/styles.css";
+// import "react-input-range/lib/css/index.css";
+import "react-perfect-scrollbar/dist/css/styles.css";
+import { Provider } from "react-redux";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import "react-responsive-modal/styles.css";
 
-// import "swiper/css";
-// import "swiper/css/navigation";
-// import StorageWrapper from "../components/ecommerce/storage-wrapper";
-// import store from "../redux/store";
-// import Preloader from "./../components/elements/Preloader";
-// import "../public/assets/css/main.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import StorageWrapper from "../components/ecommerce/storage-wrapper";
+import store from "../redux/store";
+import Preloader from "./../components/elements/Preloader";
+import "../public/assets/css/main.css";
 
 
 
@@ -20,11 +20,25 @@ function MyApp({ Component, pageProps }) {
 
     const [loading, setLoading] = useState(false);
 
-   
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+    }, []);
     return (
-        <div>
-            
-        </div>
+        <>
+            {!loading ? (
+                <Provider store={store}>
+                    <StorageWrapper>
+                        <Component {...pageProps} />
+                        <ToastContainer />
+                    </StorageWrapper>
+                </Provider>
+            ) : (
+                <Preloader />
+            )}
+        </>
     );
 }
 export default MyApp;
